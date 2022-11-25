89b6b836cbd6f4325893dba3c3d3b1ba697f65d0
jonaC <jonathancavia0101@gmail.com>
2022-11-24 21:24:23 -0300

    puli2

--- obtenerSilabas.py
+++ obtenerSilabas.py
@@ -7,14 +7,14 @@ def obtenerPalabrasQueContenganLasSilabas(silabas, palabras, maxSilabas=2):
     palabrasNew = []
     for palabra in palabras:
         cantSilabas = len(palabra["silabas"])
-        
 
         if (cantSilabas <= maxSilabas):
             silabasCopiadas = palabra["silabas"].copy()
 
-            scoresSilabas = dict(zip(palabra["silabas"],[0 for x in silabasCopiadas]))
+            scoresSilabas = dict(
+                zip(palabra["silabas"], [0 for x in silabasCopiadas]))
 
-            while(len(silabasCopiadas) > 0):
+            while (len(silabasCopiadas) > 0):
                 scoresSilabas[silabasCopiadas.pop(0)] += 1
 
             silabasAux = set(palabra["silabas"])
@@ -25,7 +25,6 @@ def obtenerPalabrasQueContenganLasSilabas(silabas, palabras, maxSilabas=2):
 
             for sil in interseccion:
                 score += scoresSilabas[sil]
-                    
 
             if (score >= 2):
                 palabrasNew.append(palabra)
@@ -53,14 +52,32 @@ with open("palabrasJson.txt", "r") as Myfile:
         # les sacamos las silabas y hacemos un set
         silabas = set(first["silabas"] + second["silabas"])
 
+        # obtenemos las palabras que se forman con estas silabas
         palabrasObtenidas = [first, second] + \
             obtenerPalabrasQueContenganLasSilabas(silabas, palabras)
+
+        conteoSilabas = {}
         silabasNew = []
 
         for palabra in palabrasObtenidas:
             silabasNew += palabra["silabas"]
 
-        silabasNew = list(set(silabasNew))
+        silabasNew = set(silabasNew)
+        conteoSilabas = dict(zip(silabasNew, [0 for sil in silabasNew]))
+
+        for palabra in palabrasObtenidas:
+            conteoPalabraActualSilabas = dict(
+                zip(palabra["silabas"], [0 for x in palabra["silabas"]]))
+
+            for silaba in palabra["silabas"]:
+                conteoPalabraActualSilabas[silaba] += 1
+
+            for key, value in conteoPalabraActualSilabas.items():
+                conteoSilabas[key] = max(conteoSilabas[key], value)
+
+        silabasNew = []
+        for key, val in conteoSilabas.items():
+            silabasNew.extend([key]*val)
 
         batch = {"silabas": silabasNew, "palabras": palabrasObtenidas}
         batches.append(batch)
